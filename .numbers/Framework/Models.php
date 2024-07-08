88d2a44ba858936c838b55addc05e8bcbe4f729f
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2024-07-08 15:13:04 -0400

    Removed .numbers

--- .numbers/Framework/Models.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$numbers_enhancement_types = [
-	'b' => 'Bug',
-	'f' => 'Feature',
-	'e' => 'Enhancement',
-	'r' => 'Refactoring',
-	'o' => 'Other',
-];
--- .numbers/README.md
+++
@@ -1,4 +0,0 @@
-# Change Log
-Change Log repository for Numbers Framework
-
-Run make version_test or make version commit.
